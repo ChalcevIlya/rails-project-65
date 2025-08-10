63db119ace13c214faf0c4d9cc3a36915d61e277
Chalcev Ilya <mr.chalcev@bk.ru>
2025-08-10 20:05:43 +0300

    Ru locale + db null fixes

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -7,6 +7,7 @@ class Web::AuthController < ApplicationController
     email = auth.dig('info', 'email').to_s.downcase
     user = User.find_or_initialize_by(email: email)
     user.name = auth.dig('info', 'name')
+    user.admin = false if user.new_record?
     user.save!
 
     session[:user_id] = user.id
