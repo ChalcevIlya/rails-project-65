e564b7e1552628791106b259f7dfd3efb6d216c3
Chalcev Ilya <mr.chalcev@bk.ru>
2025-09-25 12:25:25 +0300

    Clearing controllers and tests structure

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -11,7 +11,9 @@ class Web::ApplicationController < ApplicationController
   before_action :set_locale
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    return @current_user if defined?(@current_user)
+
+    @current_user = User.find_by(id: session[:user_id])
   end
 
   def user_signed_in?
